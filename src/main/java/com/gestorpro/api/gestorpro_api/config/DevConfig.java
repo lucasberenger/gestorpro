a2262a4b50315fd8c37608668b5c2bb6b86dc9f4
lucasberenger <thegreatberenger@gmail.com>
2025-02-07 18:09:05 -0300

    chore: add method to DevConfig for creating test user

--- src/main/java/com/gestorpro/api/gestorpro_api/config/DevConfig.java
+++ src/main/java/com/gestorpro/api/gestorpro_api/config/DevConfig.java
@@ -1,6 +1,9 @@
 package com.gestorpro.api.gestorpro_api.config;
 
 
+import com.gestorpro.api.gestorpro_api.entities.User;
+import com.gestorpro.api.gestorpro_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,8 +12,17 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevConfig implements CommandLineRunner {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("RUNNING ON DEV PROFILE");
+
+        User user1 = new User(null, "Maria Brown", "MariaBrown@io.com", "988888888", "123456");
+
+        userRepository.save(user1);
     }
+
+
 }
