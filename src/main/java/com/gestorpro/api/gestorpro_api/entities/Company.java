46837f4b3556ce547630ae48a1c29845a3ba78ed
lucasberenger <thegreatberenger@gmail.com>
2025-02-07 19:18:52 -0300

    feat: create Company Entity and add one to one relation with User

---
+++ src/main/java/com/gestorpro/api/gestorpro_api/entities/Company.java
@@ -0,0 +1,63 @@
+package com.gestorpro.api.gestorpro_api.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_company")
+public class Company implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String cnpj;
+
+    @OneToOne(mappedBy = "company")
+    private User user;
+
+    public Company() {
+    }
+
+    public Company(Long id, String name, String cnpj, User user) {
+        this.id = id;
+        this.name = name;
+        this.cnpj = cnpj;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- src/main/java/com/gestorpro/api/gestorpro_api/entities/User.java
+++ src/main/java/com/gestorpro/api/gestorpro_api/entities/User.java
@@ -19,15 +19,20 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
     public User() {
     }
 
-    public User(Long id, String name, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password, Company company) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
+        this.company = company;
     }
 
     public Long getId() {
@@ -68,5 +73,13 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
     }
 }
