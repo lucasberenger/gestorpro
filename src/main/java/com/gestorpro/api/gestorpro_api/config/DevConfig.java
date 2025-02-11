0f92052df7fec046dd8f90073e66d48ed89e9549
lucasberenger <thegreatberenger@gmail.com>
2025-02-11 17:52:49 -0300

    test: create employee benefits and add to employee1

--- src/main/java/com/gestorpro/api/gestorpro_api/config/DevConfig.java
+++ src/main/java/com/gestorpro/api/gestorpro_api/config/DevConfig.java
@@ -3,8 +3,11 @@ package com.gestorpro.api.gestorpro_api.config;
 
 import com.gestorpro.api.gestorpro_api.entities.Company;
 import com.gestorpro.api.gestorpro_api.entities.Employee;
+import com.gestorpro.api.gestorpro_api.entities.EmployeeBenefit;
 import com.gestorpro.api.gestorpro_api.entities.User;
+import com.gestorpro.api.gestorpro_api.entities.enums.Benefit;
 import com.gestorpro.api.gestorpro_api.repositories.CompanyRepository;
+import com.gestorpro.api.gestorpro_api.repositories.EmployeeBenefitsRepository;
 import com.gestorpro.api.gestorpro_api.repositories.EmployeeRepository;
 import com.gestorpro.api.gestorpro_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,9 @@ public class DevConfig implements CommandLineRunner {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private EmployeeBenefitsRepository employeeBenefitsRepository;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("RUNNING ON DEV PROFILE");
@@ -40,10 +46,15 @@ public class DevConfig implements CommandLineRunner {
         user1.setCompany(comp1);
         userRepository.save(user1);
 
-        Employee emp1 = new Employee(null, "Alex Green", "123456789", "999999999", "Analyst", 3000.0, comp1);
+        Employee emp1 = new Employee(null, "Alex Green", "123456789", "999999999", "Analyst", 3000.0, comp1, new HashSet<>());
         employeeRepository.save(emp1);
         comp1.getEmployees().add(emp1);
         companyRepository.save(comp1);
+
+        EmployeeBenefit employeeBenefit1 = new EmployeeBenefit(null, emp1, Benefit.FOOD_ALLOWANCE, 300.0);
+        EmployeeBenefit employeeBenefit2 = new EmployeeBenefit(null, emp1, Benefit.TRANSPORTATION_ALLOWANCE, 150.0);
+        emp1.getEmployeeBenefits().addAll(Set.of(employeeBenefit1, employeeBenefit2));
+        employeeRepository.save(emp1);
 
 
 
